503920c9a2d8c11f65661d67fae3d6d986c40508
Ratha CHHORM <mrrrathanj@gmail.com>
2026-09-24 23:16:03 +0700

    clean up unused imports in exception and configuration classes

--- task-management-backend/src/main/java/com/ratha/taskmanagementbackend/config/SecurityConfig.java
+++ task-management-backend/src/main/java/com/ratha/taskmanagementbackend/config/SecurityConfig.java
@@ -1,15 +1,11 @@
 package com.ratha.taskmanagementbackend.config;
 
-import com.ratha.taskmanagementbackend.jwt.CustomAuthenticationEntryPoint;
-import com.ratha.taskmanagementbackend.jwt.JwtAuthEntrypoint;
-import com.ratha.taskmanagementbackend.jwt.JwtAuthFilter;
-import com.ratha.taskmanagementbackend.jwt.UserService;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,7 +14,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import com.ratha.taskmanagementbackend.jwt.CustomAuthenticationEntryPoint;
+import com.ratha.taskmanagementbackend.jwt.JwtAuthEntrypoint;
+import com.ratha.taskmanagementbackend.jwt.JwtAuthFilter;
+import com.ratha.taskmanagementbackend.jwt.UserService;
+
+import lombok.AllArgsConstructor;
 
 @Configuration
 @AllArgsConstructor
--- task-management-backend/src/main/java/com/ratha/taskmanagementbackend/exception/GlobalException.java
+++ task-management-backend/src/main/java/com/ratha/taskmanagementbackend/exception/GlobalException.java
@@ -1,17 +1,14 @@
 package com.ratha.taskmanagementbackend.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.method.annotation.HandlerMethodValidationException;
-
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalException {
